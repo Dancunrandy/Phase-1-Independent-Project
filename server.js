48d9a1b7db1d398e00076ac4b6c988c255cd1fae
git_dancunrandy <dancunrandy@gmail.com>
2022-12-17 09:21:32 +0300

    server config

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname + '/public', {
+  setHeaders: function (res, path) {
+    if (path.indexOf('.js') !== -1) {
+      res.setHeader('Content-Type', 'application/javascript');
+    }
+  }
+}));
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
